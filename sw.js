9073cab2b4c9e1b1a919be7b846b45e48372dacf
scruffyhightops <scruffyhightops@gmail.com>
2026-09-12 13:08:41 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ecto-os-v0-2-1';
+const CACHE_NAME = 'ecto-os-v0-2-2';
 const APP_SHELL = [
   './','./index.html','./manifest.json',
   './icons/icon-192.png','./icons/icon-512.png','./icons/maskable-512.png',
