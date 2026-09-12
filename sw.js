7a192abb534521a97ceffe7c3d29a498984f157d
scruffyhightops <scruffyhightops@gmail.com>
2026-09-13 00:04:05 +0100

    Test native CONNECT PACK audio

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ecto-os-v0-3-1';
+const CACHE_NAME = 'ecto-os-v0-3-2';
 const APP_SHELL = [
   './','./index.html','./manifest.json',
   './icons/icon-192.png','./icons/icon-512.png','./icons/maskable-512.png',
