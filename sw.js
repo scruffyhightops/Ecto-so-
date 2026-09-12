94d70535349cac87e9186dd95e9bbfa5bfa09146
scruffyhightops <scruffyhightops@gmail.com>
2026-09-12 23:24:04 +0100

    Update to v0.2.8 audio diagnostic

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ecto-os-v0-2-7';
+const CACHE_NAME = 'ecto-os-v0-2-8';
 const APP_SHELL = [
   './','./index.html','./manifest.json',
   './icons/icon-192.png','./icons/icon-512.png','./icons/maskable-512.png',
